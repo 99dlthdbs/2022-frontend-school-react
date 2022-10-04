775c4b6b1bc2c28a30a3c001aa0c5e9d6157093b
Lee-SoYoon <99dlthdbs@gmail.com>
2022-10-04 22:38:47 +0900

    Update ⚡ react-query를 사용하여 queryClient 객체 생성, useQuery를 통한 data READ 구현

--- 12/my-store/src/mocks/db.js
+++ 12/my-store/src/mocks/db.js
@@ -1,15 +1,15 @@
-import { factory, primaryKey } from "@mswjs/data";
+import { factory, primaryKey } from '@mswjs/data';
 
 export const db = factory({
   user: {
     id: primaryKey(),
-    nickName: "",
-    email: "",
+    nickName: '',
+    email: '',
   },
 });
 
 db.user.create({
   id: 1,
-  nickName: "hwarari",
-  email: "hwarari@gmail.com",
+  nickName: 'leesoyoon',
+  email: '99dlthdbs@gmail.com',
 });
--- 12/my-store/src/pages/Edit.js
+++ 12/my-store/src/pages/Edit.js
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+import { getUser } from '../mocks/api';
+
+// api를 통해 현재 닉네임값 가져오기
+// handleSubmit: 업데이트 된 inputValue를 서버에 전송해서, 닉네임값 업데이트 하기
 
 export default function Edit() {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
+
+  const { data, isLoading } = useQuery('@getUser', getUser);
+  console.log(data);
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
@@ -10,14 +18,16 @@ export default function Edit() {
     e.preventDefault();
   };
 
+  if (isLoading) return <span>Loading...</span>;
+
   return (
     <>
       <h1>Edit</h1>
-      <h3>현재 닉네임: {"hwarari"}</h3>
+      <h3>현재 닉네임: {data.nickName}</h3>
       <form onSubmit={handleSubmit}>
         <label>
           변경할 닉네임:
-          <input type="text" value={inputValue} onChange={handleChange} />
+          <input type='text' value={inputValue} onChange={handleChange} />
         </label>
       </form>
     </>
