16d1570044f68270f363f3f8c15f54b4fb47567d
Lee-SoYoon <99dlthdbs@gmail.com>
2022-10-04 23:01:19 +0900

    Update ⚡ Mutations Query Invalidation

--- 12/my-store/src/pages/Edit.js
+++ 12/my-store/src/pages/Edit.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
-import { useQuery } from 'react-query';
-import { getUser } from '../mocks/api';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { getUser, updateNickname } from '../mocks/api';
 
 // api를 통해 현재 닉네임값 가져오기
 // handleSubmit: 업데이트 된 inputValue를 서버에 전송해서, 닉네임값 업데이트 하기
 
 export default function Edit() {
   const [inputValue, setInputValue] = useState('');
+  const queryClient = useQueryClient();
 
   const { data, isLoading } = useQuery('@getUser', getUser);
-  console.log(data);
+  const mutation = useMutation(updateNickname, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('@getUser');
+    },
+  });
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+    mutation.mutate(inputValue);
   };
 
   if (isLoading) return <span>Loading...</span>;
